c328a74000a32fcb163bffced8e13616323d3dca
SnrJay <judeomoirabor@gmail.com>
2024-09-16 00:46:32 +0100

    creates and adds default styling to the page

--- src/componets/login-signUp/loginSignUp.jsx
+++ src/componets/login-signUp/loginSignUp.jsx
@@ -1,7 +1,38 @@
-import "./LoginSignUp.css";
+import "./loginSignUp.css";
 
 const LoginSignUp = () => {
-  return <div></div>;
+  return (
+    <div className="container">
+      <div className="header">
+        <div className="logo">
+          <span className="material-symbols-outlined">cyclone</span>
+        </div>
+        <h1>Create account</h1>
+        <p>please enter your details</p>
+      </div>
+      <div className="inputs">
+        <div className="input">
+          <span className="material-symbols-outlined">person</span>
+          <input type="text" placeholder="username" />
+        </div>
+        <div className="input">
+          <span className="material-symbols-outlined">mail</span>
+          <input type="email" placeholder="email" />
+        </div>
+        <div className="input">
+          <span className="material-symbols-outlined">key</span>
+          <input type="password" placeholder="password" />
+        </div>
+        <div className="forgot-password">
+          Forgot password? <span>Click here</span>
+        </div>
+      </div>
+      <div className="button-container">
+        <button>Sign Up</button>
+        <button>Login</button>
+      </div>
+    </div>
+  );
 };
 
 export default LoginSignUp;
